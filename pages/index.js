a0e79fdb80834223b74c773a1517da6208e96d38
Inderjit Shahi <inderjitshahi_mc20a7_13@dtu.ac.in>
2022-12-26 16:51:31 +0530

    added header footer and banner

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Banner from '../components/Banner';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 
-export default function Home() {
+export default function Home(props) {
+  // console.log(props);
+  const data = Object.values(props);
+
+  console.log(data);
+  const fun=(e)=>{
+    console.log(e.target.textContent);
+  }
   return (
     <>
       <Head>
@@ -11,9 +21,19 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className='min-h-screen flex items-center justify-center text-5xl text-purple-600 font-bold'>
-        <h1>Milaan</h1>
-      </div>
+      <Header/>
+      <Banner/>
+      <Footer/>
+
     </>
   )
 }
+
+// export async function getStaticProps() {
+//   const res = await fetch('https://test.turbocare.app/turbo/care/v1/makes?class=2w');
+//   const users = await res.json();
+//   // console.log(users);
+//   return {
+//     props: { ...users }
+//   }
+// }
