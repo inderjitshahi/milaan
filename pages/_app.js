1ec02c69b867a042132938d83416cfdcb350e4df
Inderjit Shahi <inderjitshahi_mc20a7_13@dtu.ac.in>
2022-12-28 17:19:33 +0530

    firxed curasol

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,16 @@
+import { useEffect } from 'react';
 import '../styles/globals.css'
-import 'tw-elements';
+function MyApp({ Component, pageProps }) {
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+
+  useEffect(() => {
+  const use = async () => {
+    (await import('tw-elements')).default;
+      };
+      use();
+    }, []);
+  
+    return <Component {...pageProps} />;
+  }
+  
+  export default MyApp;
